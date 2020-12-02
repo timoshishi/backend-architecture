41697a0dde2e61ba31a26c943eb4819d5ba5b724
timoshishi <timfrrst@gmail.com>
2020-12-02 16:35:57 -0500

    Finished stubbed-api and dummy data

--- client/src/Parse.js
+++ client/src/Parse.js
@@ -3,102 +3,125 @@ import $ from 'jquery';
 const Parse = {
   server: 'http://52.26.193.201:3000/reviews/4',
 
-  getAllList: function(successCB, errorCB = null) {
+  getAllList: function (successCB, errorCB = null) {
     $.ajax({
       url: Parse.server + '/list?count=100&sort=relevant',
       type: 'GET',
       contentType: 'application/json',
       data: {},
       success: successCB,
-      error: errorCB || function() {
-        console.error('Failed to fetch list of reviews sorted by relevance from Greenfield')
-      }
+      error:
+        errorCB ||
+        function () {
+          console.error(
+            'Failed to fetch list of reviews sorted by relevance from Greenfield'
+          );
+        },
     });
   },
-  getAllListHelpfulness: function(successCB, errorCB = null) {
+  getAllListHelpfulness: function (successCB, errorCB = null) {
     $.ajax({
       url: Parse.server + '/list?count=100&sort=helpfulness',
       type: 'GET',
       contentType: 'application/json',
       data: {},
       success: successCB,
-      error: errorCB || function() {
-        console.error('Failed to fetch list of reviews sorted by helpfulness from Greenfield')
-      }
+      error:
+        errorCB ||
+        function () {
+          console.error(
+            'Failed to fetch list of reviews sorted by helpfulness from Greenfield'
+          );
+        },
     });
   },
-  getAllListNewest: function(successCB, errorCB = null) {
+  getAllListNewest: function (successCB, errorCB = null) {
     $.ajax({
       url: Parse.server + '/list?count=100&sort=newest',
       type: 'GET',
       contentType: 'application/json',
       data: {},
       success: successCB,
-      error: errorCB || function() {
-        console.error('Failed to fetch list of reviews sorted by newest from Greenfield')
-      }
+      error:
+        errorCB ||
+        function () {
+          console.error(
+            'Failed to fetch list of reviews sorted by newest from Greenfield'
+          );
+        },
     });
   },
-  getProductMeta: function(successCB, errorCB = null) {
+  getProductMeta: function (successCB, errorCB = null) {
     $.ajax({
       url: Parse.server + '/meta',
       type: 'GET',
       contentType: 'application/json',
       data: {},
       success: successCB,
-      error: errorCB || function() {
-        console.error('Failed to fetch meta of reviews from Greenfield')
-      }
+      error:
+        errorCB ||
+        function () {
+          console.error('Failed to fetch meta of reviews from Greenfield');
+        },
     });
-  } ,
-  markAsHelpful: function(id, successCB, errorCB = null) {
+  },
+  markAsHelpful: function (id, successCB, errorCB = null) {
     $.ajax({
       url: `http://52.26.193.201:3000/reviews/helpful/${id}`,
       type: 'PUT',
       contentType: 'application/json',
       data: {},
       success: successCB,
-      error: errorCB || function() {
-        console.error('Failed to mark review as helpful on Greenfield')
-      }
-    })
+      error:
+        errorCB ||
+        function () {
+          console.error('Failed to mark review as helpful on Greenfield');
+        },
+    });
   },
-  submitReview: function(obj, successCB, errorCB = null) {
+  submitReview: function (obj, successCB, errorCB = null) {
+    console.log(JSON.stringify(obj));
     $.ajax({
       url: Parse.server,
       type: 'Post',
       contentType: 'application/json',
       data: obj,
       success: successCB,
-      error: errorCB || function() {
-        console.error('Error when submiting review on Greenfield')
-      }
+      error:
+        errorCB ||
+        function () {
+          console.error('Error when submiting review on Greenfield');
+        },
     });
   },
-  getAllProductList: function(successCB, errorCB = null) {
+  getAllProductList: function (successCB, errorCB = null) {
     $.ajax({
       url: 'http://52.26.193.201:3000/products/list?count=20',
       type: 'GET',
       contentType: 'application/json',
       data: {},
       success: successCB,
-      error: errorCB || function() {
-        console.error('Failed to fetch list of products from Greenfield')
-      }
+      error:
+        errorCB ||
+        function () {
+          console.error('Failed to fetch list of products from Greenfield');
+        },
     });
   },
-  reportReview: function(id, successCB, errorCB = null) {
+  reportReview: function (id, successCB, errorCB = null) {
     $.ajax({
       url: `http://52.26.193.201:3000/reviews/report/${id}`,
       type: 'PUT',
       contentType: 'application/json',
       data: {},
       success: successCB,
-      error: errorCB || function() {
-        console.error('Failed to report review on Greenfield')
-      }
-    })
+      error:
+        errorCB ||
+        function () {
+          console.error('Failed to report review on Greenfield');
+        },
+    });
   },
-}
+};
 
-export default Parse
+export default Parse;
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('products'));
+router.get('/products/list', async (req, res) => {
+  const { count, page } = req.query;
+  count = count || 10;
+  page = page || 10;
+  //try catch
+  // SELECT * FROM product WHERE page = ${page} LIMIT ${count}
+  //Product.find({}).where({page}).equals(${page}).limit(${count})
+
+  //res.json(resultObj)
+});
 
 module.exports = router;
